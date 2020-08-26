3723b004b11d30cc7b56f979888d2c1bd6a3f291
qsparks <q.m.sparks@gmail.com>
2020-08-26 17:28:12 -0500

    creates base files and begins early logic exploration

---
+++ scripts/app.js
@@ -0,0 +1,89 @@
+const deck = [
+  {
+    name: "Bulbasaur",
+    damage: 60
+  }, {
+    name: "Caterpie",
+    damage: 40
+  }, {
+    name: "Charmander",
+    damage: 60
+  }, {
+    name: "Clefairy",
+    damage: 50
+  }, {
+    name: "Jigglypuff",
+    damage: 60
+  }, {
+    name: "Mankey",
+    damage: 30
+  }, {
+    name: "Meowth",
+    damage: 60
+  }, {
+    name: "Nidoran - female",
+    damage: 60
+  }, {
+    name: "Nidoran - male",
+    damage: 50
+  }, {
+    name: "Oddish",
+    damage: 40
+  }, {
+    name: "Pidgey",
+    damage: 50
+  }, {
+    name: "Pikachu",
+    damage: 50
+  }, {
+    name: "Poliwag",
+    damage: 50
+  }, {
+    name: "Psyduck",
+    damage: 60
+  }, {
+    name: "Rattata",
+    damage: 30
+  }, {
+    name: "Squirtle",
+    damage: 60
+  }, {
+    name: "Vulpix",
+    damage: 50
+  }, {
+    name: "Weedle", 
+    damage: 40
+  }
+];
+
+
+class Game {
+  /* Properties */
+  // The Deck
+  // Players
+  // Total score
+
+  /* Methods */
+  shuffle() {
+
+  }
+}
+
+class Round extends Game { 
+/* Properties */
+// Subscore
+
+/* Methods */
+deal() {
+
+}
+
+}
+
+class Player {
+/* Properties */
+// Hand
+
+/* Methods */
+
+}
