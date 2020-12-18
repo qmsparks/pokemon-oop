f5b89f77ff738e9e75e620369c6c3a4b0163aba0
qsparks <q.m.sparks@gmail.com>
2020-12-17 23:49:36 -0600

    lays down extremely early logic structure thoughts, but I am fully prepared to pitch these when I look at them with more alert eyes

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+/* classes to create, from smallest to largest (roughly):
+card
+deck - draw, discard, each player's hand (may or may not need to actually extend these classes)
+player
+game
+*/
+
+class Card {
+
+}
+
+class Deck {
+
+}
+
+class Player {
+
+}
+
+class Game {
+  
+}
